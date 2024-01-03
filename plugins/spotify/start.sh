3692e2e8ce055657d5e03e7bd9bb049a409b7cf4
Mattia Calabrese <48023568+MattiaCalabrese@users.noreply.github.com>
2024-01-03 19:00:14 +0100

    Update start.sh

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -8,7 +8,8 @@ fi
 # --- ENV VARS ---
 # SOUND_DEVICE_NAME: Set the device broadcast name for Spotify
 # SOUND_SPOTIFY_BITRATE: Set the playback bitrate
-SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
+# SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
+SOUND_DEVICE_NAME=${RESIN_DEVICE_NAME_AT_INIT:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
 
 # SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalization
