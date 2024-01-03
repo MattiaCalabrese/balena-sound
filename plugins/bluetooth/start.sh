d73908fcef8421eee5200b74405304c7072f1429
Mattia Calabrese <48023568+MattiaCalabrese@users.noreply.github.com>
2024-01-03 18:59:31 +0100

    Update start.sh

--- plugins/bluetooth/start.sh
+++ plugins/bluetooth/start.sh
@@ -5,4 +5,6 @@ if [[ -n "$SOUND_DISABLE_BLUETOOTH" ]]; then
   exit 0
 fi
 
+BLUETOOTH_DEVICE_NAME=${RESIN_DEVICE_NAME_AT_INIT:-"balenaSound Bluetooth $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
+
 exec /usr/src/bluetooth-agent
